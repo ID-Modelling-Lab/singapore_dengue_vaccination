48838879102cccdbfffaaa3451c1f35fd1c4dba1
Abhishek Senapati <abhishekiz4u04@gmail.com>
2024-09-20 10:32:58 +0800

    intermediate changes

--- code/projection/run_vac_projection.R
+++ code/projection/run_vac_projection.R
@@ -72,6 +72,208 @@ death_mos = 1/10
 lambda_mos = death_mos 
 
 
+
+get_ve_point_estimate_from_trial <- function(df_efficacy) {
+  ## Filter the VE based on overall and serostatus stratified but serotype stratified
+  df_no_sero <- df_efficacy %>% filter(ve_against %in% 
+                                         c("vcd_overall", "hosp_overall", "vcd_sp", "vcd_sn", "hosp_sp", "hosp_sn"))
+  
+  ## create two groups based VCD and Hosp & Overall or serostatus stratified
+  
+  df_no_sero_strat <- df_no_sero %>% 
+    mutate( group1 = factor(case_when(
+      ve_against %in% c("vcd_overall", "vcd_sp", "vcd_sn") ~ "VCD",
+      ve_against %in% c("hosp_overall","hosp_sp", "hosp_sn") ~ "HOSPITALIZED"), 
+      levels = c("VCD", "HOSPITALIZED")),
+      
+      group2 = factor(case_when(
+        ve_against %in% c("vcd_overall", "hosp_overall") ~ "Overall",
+        ve_against %in% c("vcd_sp","hosp_sp") ~ "Sero+",
+        ve_against %in% c("vcd_sn","hosp_sn") ~ "Sero-"),
+        levels = c("Overall", "Sero+", "Sero-"))
+    )
+  
+  
+  ## Now first we remove the values where VE == 100 and replace with the previous and next values
+  ## Follow the same for the C.I values
+  ## There is only one such instance
+  # the value of ve which will replace ve==100
+  replace_ve_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn", month == 36) %>%
+    pull(ve)
+  
+  # the value of lower bound
+  replace_velow_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn", month == 36) %>%
+    pull(ve_low)
+  
+  # the value of upper bound
+  replace_veup_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn", month == 36) %>%
+    pull(ve_up)
+  
+  
+  # Modify the dataframe now replacing the VE==100 with above values
+  df_no_sero_strat <- df_no_sero_strat %>%
+    mutate(
+      ## Replace where VE value is 100 with the previous or next value
+      ve = ifelse(
+        group1 == "HOSPITALIZED" & group2 %in% c("Sero-") & ve == 100,
+        replace_ve_hosp_sn,
+        ve
+      ),
+      
+      ve_low = ifelse(
+        group1 == "HOSPITALIZED" &
+          group2 %in% c("Sero-") & is.na(ve_low) == TRUE,
+        replace_velow_hosp_sn,
+        ve_low
+      ),
+      
+      ve_up = ifelse(
+        group1 == "HOSPITALIZED" &
+          group2 %in% c("Sero-") & is.na(ve_up) == TRUE,
+        replace_veup_hosp_sn,
+        ve_up
+      )
+    )
+  
+  
+  ## Filter the VE based on serotypes
+  
+  df_sero <- df_efficacy %>%  filter(grepl("_dv[1234]$", ve_against))
+  
+  ## Create 3 groups
+  df_sero_strat <- df_sero %>%
+    mutate(
+      group1 = factor(case_when(
+        grepl("_dv1", ve_against, ignore.case = TRUE) ~ "DENV1",
+        grepl("_dv2", ve_against, ignore.case = TRUE) ~ "DENV2",
+        grepl("_dv3", ve_against, ignore.case = TRUE) ~ "DENV3",
+        grepl("_dv4", ve_against, ignore.case = TRUE) ~ "DENV4"),
+        levels = c("DENV1", "DENV2", "DENV3", "DENV4")),
+      
+      group2 = factor(case_when(
+        grepl("_sp_", ve_against, ignore.case = TRUE) ~ "Sero+",
+        grepl("_sn_", ve_against, ignore.case = TRUE) ~ "Sero-"),
+        levels = c("Sero+", "Sero-")),
+      
+      group3 = factor(case_when(
+        grepl("vcd_", ve_against, ignore.case = TRUE) ~ "VCD",
+        grepl("hosp_", ve_against, ignore.case = TRUE) ~ "Hospitalized"),
+        levels = c("VCD", "Hospitalized"))
+      
+    )
+  
+  ### Now from first data frame we will pull out the estimated of VE against HOSPITALIZED both for Sero+ and Sero-
+  # Sero+
+  pull_from_no_sero_ve_hosp_sp <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sp") %>%
+    pull(ve)
+  
+  pull_from_no_sero_velow_hosp_sp <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sp") %>%
+    pull(ve_low)
+  
+  pull_from_no_sero_veup_hosp_sp <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sp") %>%
+    pull(ve_up)
+  
+  # Sero- 
+  pull_from_no_sero_ve_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn") %>%
+    pull(ve)
+  
+  pull_from_no_sero_velow_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn") %>%
+    pull(ve_low)
+  
+  pull_from_no_sero_veup_hosp_sn <- df_no_sero_strat %>%
+    filter(ve_against == "hosp_sn") %>%
+    pull(ve_up)
+  
+  ## replace where the ve is 100 with next or previous values
+  # first instance
+  replace_ve_vcd_sp_dv4 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sp_dv4", month == 36) %>%
+    pull(ve)
+  
+  replace_velow_vcd_sp_dv4 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sp_dv4", month == 36) %>%
+    pull(ve_low)
+  
+  replace_veup_vcd_sp_dv4 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sp_dv4", month == 36) %>%
+    pull(ve_up)
+  
+  # second instance
+  replace_ve_vcd_sn_dv2 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sn_dv2", month == 24) %>%
+    pull(ve)
+  
+  replace_velow_vcd_sn_dv2 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sn_dv2", month == 24) %>%
+    pull(ve_low)
+  
+  replace_veup_vcd_sn_dv2 <- df_sero_strat %>%
+    filter(ve_against == "vcd_sn_dv2", month == 24) %>%
+    pull(ve_up)
+  
+  # Modify df2 for hospitalization of DENV3 and DENV4 for sero+ and sero-
+  df_sero_strat <- df_sero_strat %>%
+    mutate(
+      
+      ## Ve against hosp for denv3 and denv4 both sero+ sero- are zero
+      ve = ifelse(group3 == "Hospitalized" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero+", "Sero-"), 0, ve),
+      ve_low = ifelse(group3 == "Hospitalized" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero+", "Sero-"), 0, ve_low),
+      ve_up = ifelse(group3 == "Hospitalized" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero+", "Sero-"), 0, ve_up),
+      
+      ## VE against VCD for DENV3 and DENV4 for Sero- are zero
+      ve = ifelse(group3 == "VCD" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero-"), 0, ve),
+      ve_low = ifelse(group3 == "VCD" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero-"), 0, ve_low),
+      ve_up = ifelse(group3 == "VCD" & group1 %in% c("DENV3", "DENV4") & group2 %in% c("Sero-"), 0, ve_up),
+      
+      ## Replace where VE value is 100 with the previous or next value
+      # first instance of VE == 100
+      ve = ifelse( group3 == "VCD" & group1 %in% c("DENV4") & group2 %in% c("Sero+") & ve == 100,
+                   replace_ve_vcd_sp_dv4, ve),
+      ve_low = ifelse( group3 == "VCD" & group1 %in% c("DENV4") & group2 %in% c("Sero+") & is.na(ve_low) == TRUE, 
+                       replace_velow_vcd_sp_dv4, ve_low),
+      ve_up = ifelse( group3 == "VCD" & group1 %in% c("DENV4") & group2 %in% c("Sero+") & is.na(ve_up) == TRUE,
+                      replace_veup_vcd_sp_dv4, ve_up),
+      
+      ## second instance of VE == 100    
+      ve = ifelse( group3 == "VCD" & group1 %in% c("DENV2") & group2 %in% c("Sero-") & ve == 100, 
+                   replace_ve_vcd_sn_dv2, ve),
+      ve_low = ifelse( group3 == "VCD" & group1 %in% c("DENV2") & group2 %in% c("Sero-") & is.na(ve_low) == TRUE,
+                       replace_velow_vcd_sn_dv2, ve_low),
+      ve_up = ifelse( group3 == "VCD" & group1 %in% c("DENV2") & group2 %in% c("Sero-") & is.na(ve_up) == TRUE, 
+                      replace_veup_vcd_sn_dv2, ve_up),
+      
+      # Replace hospitalized VE from the unstratified ve estimate
+      # seropositive
+      ve = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1", "DENV2") & group2 %in% c("Sero+") ,
+                   pull_from_no_sero_ve_hosp_sp, ve),
+      ve_low = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1","DENV2") & group2 %in% c("Sero+") ,
+                       pull_from_no_sero_velow_hosp_sp, ve_low),
+      ve_up = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1","DENV2") & group2 %in% c("Sero+") ,
+                      pull_from_no_sero_veup_hosp_sp, ve_up),
+      
+      # Seronegative
+      ve = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1", "DENV2") & group2 %in% c("Sero-"),
+                   pull_from_no_sero_ve_hosp_sn, ve),
+      ve_low = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1","DENV2") & group2 %in% c("Sero-") ,
+                       pull_from_no_sero_velow_hosp_sn, ve_low),
+      ve_up = ifelse( group3 == "Hospitalized" & group1 %in% c("DENV1","DENV2") & group2 %in% c("Sero-") , 
+                      pull_from_no_sero_veup_hosp_sn, ve_up)
+    )
+  
+  return(df_sero_strat)
+}
+
+
+
+
 ## Vaccine related paramter
 get_ve_from_trial <- function(df_efficacy) {
   ## Filter the VE based on overall and serostatus stratified but serotype stratified
@@ -434,6 +636,17 @@ get_conditional_ve <- function(df_efficacy, n_vac_stage,
               hos_p = effi_hosp_sero_p_array))
   
 }
+
+
+
+
+
+
+
+
+
+
+
 
 ## Number of serotype
 n_sero <- 4
